6f6d8f18a5532ac9dc01951cfc8082856d8c7e35
Brianca Knight <briancaknight@gmail.com>
2024-02-16 10:47:51 -0800

    add detail route and view to treat

--- Bakery/Controllers/TreatController.cs
+++ Bakery/Controllers/TreatController.cs
@@ -45,5 +45,15 @@ namespace Bakery.Controllers
          return RedirectToAction("Index");
       }
     }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats
+                            // .Include(treat => treat.JoinEntities)
+                            // .ThenInclude(join => join.Flavor)
+                            .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
   }
 }
