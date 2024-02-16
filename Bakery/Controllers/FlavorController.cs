365e29e0fb5c7000831f5bd00f376cfc93bc86c1
Brianca Knight <briancaknight@gmail.com>
2024-02-16 11:11:09 -0800

    add deleteJoin route to flavor

--- Bakery/Controllers/FlavorController.cs
+++ Bakery/Controllers/FlavorController.cs
@@ -111,5 +111,14 @@ namespace Bakery.Controllers
       }
       return RedirectToAction("Details", new { id = flavor.FlavorId });
     }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      FlavorTreat joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreats.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
